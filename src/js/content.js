91152493b7c039205bcceb9e530ac1b710682b50
Suguru Murakami <sugumura@gmail.com>
2025-05-15 20:17:49 +0900

    Fix load more

--- src/js/content.js
+++ src/js/content.js
@@ -4,7 +4,7 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("onMessage", request)
 
     if (request.text === "load-more") {
-        let buttons = Array.from(document.getElementsByClassName("ajax-pagination-btn"))
+        let buttons = Array.from(document.querySelectorAll('[data-testid="issue-timeline-load-more-load-top"]'))
         if (buttons.length > 0) {
             ArrayPlusDelay(buttons, (button) => {
                 button.click()
